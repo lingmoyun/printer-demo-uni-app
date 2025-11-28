73655fd35ee1713c69772486cc6b83568898db57
winfordguo <winfordguo@gmail.com>
2025-11-28 14:38:36 +0800

    update

--- printerdemo/util/bluetooth.js
+++ printerdemo/util/bluetooth.js
@@ -125,7 +125,9 @@ const _wxWriteBLECharacteristicValue = ({
 const sleep = (time) => new Promise((resolve, reject) => setTimeout(resolve, time));
 
 // 写数据
-const _writeBLECharacteristicValue = async (deviceId, serviceId, characteristicId, value, mtu = 20) => {
+const _writeBLECharacteristicValue = async (deviceId, serviceId, characteristicId, value, mtu) => {
+    // iOS: 10000; Android: 20
+    mtu = mtu || (uni.getSystemInfoSync().platform === 'ios' ? 10000 : 20);
     const total = value.byteLength;
     console.log("================mtu=" + mtu, new Date())
     let num = 0;
@@ -436,9 +438,7 @@ const connect = async ({
     }
 
     // 写方法
-    // device.write = async (value, {
-    // 	mtu = 20
-    // }) => {
+    // device.write = async (value, mtu) => {
     // 	// console.log(device.deviceId, 'write', HEX.ab2hex(value));
     // 	if (device.writeCharacteristicId) {
     // 		await _writeBLECharacteristicValue(device.deviceId, device.serviceId, device
